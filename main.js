150786502ef7fa089e68540d76e4b8e0b16bdf1e
Danil <danilom.belem7@gmail.com>
2023-01-12 21:50:15 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,101 @@
+
+
+
+
+
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "close_white_36dp.svg";
+    }
+}
+
+
+
+
+
+
+
+
+
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+  
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+  
+  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function() {
+    cw[i].className = 'letter out';
+  }, i*80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function() {
+    nw[i].className = 'letter in';
+  }, 340+(i*80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+  
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
+
+
+
+
+
+
+function star(t, name, rm){ 
+    for(let i = 0; i < t; i ++){ //loop for how much stars
+      const c = document.getElementById('circle'); 
+      const star = document.createElement('div');
+      star.classList.add(name);
+      star.style.left = Math.random() * 100 + "%";
+      star.style.top = Math.random() * 100 + "%";
+      c.appendChild(star); // add the  star to element
+      setTimeout(() => { // remove the star after 4s
+      star.remove(); 
+    }, rm);
+    }
+  }
+  // in sit the setInterval : function, how long the for new stars, how much stars, className
+  setInterval(star, 2000, 200, 'stars', 4000); 
+  setInterval(star, 2000, 1, 'noordstar', 3000);
